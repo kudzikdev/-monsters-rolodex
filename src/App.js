f0cc056abdedd53637bf5fbd4f257634e95174af
kudzikdev <kudzikdev@gmail.com>
2019-09-23 15:29:33 +0200

    230920191529

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    monsters: [
+      {
+        id: 1,
+        name: "Frankenstein"
+      },
+      {
+        id: 2,
+        name: "Dracula"
+      },
+      {
+        id: 3,
+        name: "Zombie"
+      }
+    ]
+  };
+
+  
+
+  render() {
+    return (
+      <div className="app">
+        {this.state.monsters.map(monster => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
